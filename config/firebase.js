ca0280929f585fc1251bd71fea0ccf6ae2105018
chrisli4 <chrisli41@gmail.com>
2019-02-26 17:57:20 -0800

    initial commit

---
+++ config/firebase.js
@@ -0,0 +1,8 @@
+export default
+    apiKey: "AIzaSyAKvFlBUe3onowmFKgsIdLTa69ilBC2_CI",
+    authDomain: "plogbase.firebaseapp.com",
+    databaseURL: "https://plogbase.firebaseio.com",
+    projectId: "plogbase",
+    storageBucket: "plogbase.appspot.com",
+    messagingSenderId: "801838325251"
+};
